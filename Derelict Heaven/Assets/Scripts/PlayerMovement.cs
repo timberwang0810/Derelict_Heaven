0a14e04c52fe9f7cbb36d747a1fe665e9d350068
jiwooy <jiwyoo629@gmail.com>
2021-03-11 22:35:15 -0500

    basic movement

---
+++ Derelict Heaven/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float speed;
+    private float horizontalMove = 0.0f;
+    private bool jump = false;
+
+    bool dir = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (horizontalMove > 0.0f)
+        {
+            dir = false;
+        }
+        if (horizontalMove < 0.0f)
+        {
+            dir = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        if (jump)
+        {
+            Vector3 curPos = transform.position;
+            curPos.y += 0.1f;
+            transform.position = curPos;
+        }
+        jump = false;
+    }
+}
