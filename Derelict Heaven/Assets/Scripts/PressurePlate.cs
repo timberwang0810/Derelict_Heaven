b58f29894dc2384825e76ef2f8babb8dac46abfe
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-03-19 23:53:13 -0400

    added simple plate triggering enemies

---
+++ Derelict Heaven/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    private bool activated = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!activated && collision.gameObject.CompareTag("Enemy") && collision.gameObject.GetComponent<Enemy>().GetForm() == GameManager.Form.pressurizer)
+        {
+            activated = true;
+            Debug.Log("activated");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        activated = false;
+    }
+}
