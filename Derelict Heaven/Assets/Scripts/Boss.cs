5409f3c518d5555c77f404ce26a4aa3ac6172a05
jiwooy <jiwyoo629@gmail.com>
2021-04-06 03:23:23 -0400

    boss, background filling

---
+++ Derelict Heaven/Assets/Scripts/Boss.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int chainsDown = 0;
+    public int maxChains;
+    public GameObject winTrigger;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addChainDown()
+    {
+        chainsDown++;
+        if (chainsDown == maxChains)
+        {
+            winTrigger.SetActive(true);
+        }
+    }
+
+    public void playOpen()
+    {
+        animator.SetTrigger("open");
+    }
+
+    public void playFall()
+    {
+        animator.SetTrigger("fall");
+    }
+}
--- Derelict Heaven/Assets/Scripts/CameraManager.cs
+++ Derelict Heaven/Assets/Scripts/CameraManager.cs
@@ -7,6 +7,7 @@ public class CameraManager : MonoBehaviour
     public static CameraManager S;
     public Camera cam;
     public GameObject player;
+    public GameObject boss;
     private Vector3 origPos;
     private Vector3 moveTo;
     private bool isMoving;
@@ -40,18 +41,51 @@ public class CameraManager : MonoBehaviour
         }
     }
 
-    public void SolvePuzzlePan(GameObject door, float animTime)
+    public void ChainPan(GameObject door, GameObject chain, float animTime)
     {
-        StartCoroutine(PanCamera(door, animTime));
+        StartCoroutine(ChainPanCamera(door, chain, animTime));
     }
 
-    private IEnumerator PanCamera(GameObject door, float animTime)
+    private IEnumerator ChainPanCamera(GameObject door, GameObject chain, float animTime)
+    {
+        origPos = cam.transform.position;
+        yield return new WaitForSeconds(animTime);
+        cam.GetComponent<CameraFollow>().toggleResticted(false);
+        cam.transform.position = new Vector3(1, -2, -15);
+        GameManager.S.gameState = GameManager.GameState.paused;
+        yield return new WaitForSeconds(0.75f);
+        door.GetComponent<Door>().PlayDeactivateAnim();
+        yield return new WaitForSeconds(0.25f);
+        foreach (Transform child in chain.transform)
+        {
+            child.gameObject.GetComponent<Rigidbody2D>().gravityScale = Random.Range(0.2f, 0.7f);
+        }
+        Destroy(chain, 10);
+        boss.GetComponent<Boss>().addChainDown();
+        if (boss.GetComponent<Boss>().chainsDown == boss.GetComponent<Boss>().maxChains)
+        {
+            yield return new WaitForSeconds(2f);
+            boss.GetComponent<Boss>().playFall();
+            yield return new WaitForSeconds(3.5f);
+        }
+        yield return new WaitForSeconds(2f);
+        GameManager.S.gameState = GameManager.GameState.playing;
+        cam.GetComponent<CameraFollow>().toggleResticted(true);
+        cam.transform.position = origPos;
+    }
+
+    public void SolvePuzzlePan(GameObject door, float animTime, float z)
+    {
+        StartCoroutine(PanCamera(door, animTime, z));
+    }
+
+    private IEnumerator PanCamera(GameObject door, float animTime, float z)
     {
         origPos = cam.transform.position;
         yield return new WaitForSeconds(animTime);
         isMoving = true;
         cam.GetComponent<CameraFollow>().toggleResticted(false);
-        PanTo(door.transform.position);
+        PanTo(door.transform.position, z);
         GameManager.S.gameState = GameManager.GameState.paused;
         yield return new WaitForSeconds(0.75f);
         door.GetComponent<Door>().PlayDeactivateAnim();
@@ -62,8 +96,8 @@ public class CameraManager : MonoBehaviour
         cam.transform.position = origPos;
     }
 
-    private void PanTo(Vector3 pos)
+    private void PanTo(Vector3 pos, float z)
     {
-        moveTo = new Vector3(pos.x, pos.y, -5);
+        moveTo = new Vector3(pos.x, pos.y, z);
     }
 }
