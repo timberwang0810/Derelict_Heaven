47f8c5b9120903289b5bee00b157e3beab9e90de
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-04-06 21:05:40 -0400

    polished game winning trigger

--- Derelict Heaven/Assets/Scripts/WinningTrigger.cs
+++ Derelict Heaven/Assets/Scripts/WinningTrigger.cs
@@ -6,6 +6,6 @@ public class WinningTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player") GameManager.S.OnLevelComplete();
+        if (collision.gameObject.tag == "Player" && GameManager.S.gameState == GameManager.GameState.playing) GameManager.S.OnLevelComplete();
     }
 }
